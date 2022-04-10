ae350bd389eb12b2a7c3d4e9574b6e68abb9f0fc
meghark <98137756+meghark@users.noreply.github.com>
2022-04-09 18:06:27 -0700

    Refined unit tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,11 +2,43 @@ const Engineer = require('../lib/Engineer.js');
 
 describe('Engineer',function() {
     it('Verify creation of engineer object', () =>{
-        const engineer = new Engineer('Deena',1 , 'dummy@email.com', '888-888-888');
+        const engineer = new Engineer('Deena',1 , 'dummy@email.com', 'githubrepo');
 
         expect(engineer.name).toBe('Deena');
         expect(engineer.id).toBe(1);
         expect(engineer.email).toBe('dummy@email.com');
-        expect(engineer.onum).toBe('888-888-888');
+        expect(engineer.github).toBe('githubrepo');
     } )
+
+    it('Verify duplicate employee id is not allowed', function(){
+        const employee1 = new Engineer('Dane',2, 'dummy1.email.com');
+        const employee2 = new Engineer('James',2, 'dummy2.email.com');
+
+        // create error object, test for error.
+    })
+
+    it('get employee name', function(){
+        const emp = new Engineer('Drew',3, 'dummy3@email.com');
+        expect(emp.getName()).toBe('Drew');        
+    });
+
+    it('get employee id', function(){
+        const emp = new Engineer('Drew4',4, 'dummy4@email.com');
+        expect(emp.getId()).toBe(4);        
+    });
+
+    it('get employee email', function(){
+        const emp = new Engineer('Drew5',5, 'dummy5@email.com');
+        expect(emp.getEmail()).toBe('dummy5@email.com');        
+    });
+
+    it('get role', function(){
+        const emp = new Engineer('Drew6',6, 'dummy6@email.com');
+        expect(emp.getRole()).toBe('Engineer');        
+    });
+    
+    it('get role', function(){
+        const emp = new Engineer('Drew6',6, 'dummy6@email.com', 'repo1');
+        expect(emp.getGithub()).toBe('repo1');        
+    });
 });
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,11 +2,43 @@ const Intern = require('../lib/Intern.js');
 
 describe('Intern',function() {
     it('Verify creation of intern object', () =>{
-        const intern = new Intern('James',2 , 'dummy1@email.com', '887-888-888');
+        const intern = new Intern('James',2 , 'dummy1@email.com', 'Test School');
 
         expect(intern.name).toBe('James');
         expect(intern.id).toBe(2);
         expect(intern.email).toBe('dummy1@email.com');
-        expect(intern.onum).toBe('887-888-888');
+        expect(intern.school).toBe('Test School');
     } )
+
+    it('Verify duplicate employee id is not allowed', function(){
+        const employee1 = new Intern('Dane',2, 'dummy1.email.com');
+        const employee2 = new Intern('James',2, 'dummy2.email.com');
+
+        // create error object, test for error.
+    })
+
+    it('get employee name', function(){
+        const emp = new Intern('James',2 , 'dummy1@email.com', 'Test School');
+        expect(emp.getName()).toBe('James');        
+    });
+
+    it('get employee id', function(){
+        const emp = new Intern('James',2 , 'dummy1@email.com', 'Test School');
+        expect(emp.getId()).toBe(2);        
+    });
+
+    it('get employee email', function(){
+        const emp = new Intern('James',2 , 'dummy1@email.com', 'Test School');
+        expect(emp.getEmail()).toBe('dummy1@email.com');        
+    });
+
+    it('get role', function(){
+        const emp = new Intern('James',2 , 'dummy1@email.com', 'Test School');
+        expect(emp.getRole()).toBe('Intern');        
+    });
+
+    it('get school', function(){
+        const emp = new Intern('James',2 , 'dummy1@email.com', 'Test School1');
+        expect(emp.getSchool()).toBe('Test School1');        
+    });
 });
